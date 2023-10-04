cd82c6be29463a0684a264992fa3dd80a2c69ec5
Ayyappa-Naga <134542493+Ayyappa-Naga@users.noreply.github.com>
2023-10-04 12:53:40 +0530

    Ayyappa_Naga New Commit with new print statement (#1)
    * Ayyappa_Naga New Commit with new print statement
    
    * new One
    
    ---------
    
    Co-authored-by: AyyappaNaga <naninaga07@gmail.com>

--- Github/src/Prajwal/Program1.java
+++ Github/src/Prajwal/Program1.java
@@ -3,5 +3,11 @@ package Prajwal;
 public class Program1 {
 	public void m1() {
 		System.out.println("code pushed by prajwal");
+		
+	}
+	
+	public void method() {
+	
+		System.out.println("code pushed by AyyappaNaga");
 	}
 }
